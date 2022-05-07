bee41dda730f509ed1a066f088b9f5bdcbcecc36
Maxim Tsyganov <0xc00000f@gmail.com>
2022-05-07 13:00:12 +0300

    Initial Commit

---
+++ involta-job-placement-assigment/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  involta-job-placement-assigment
+//
+//  Created by Maxim Tsyganov on 07.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray2
+    }
+
+}
