fddef2874cef3ceccf2f7aa8ef0e426352fa3d62
Maxim Tsyganov <0xc00000f@gmail.com>
2022-05-12 18:10:27 +0300

    added refresh control, deleted loading section

--- involta-job-placement-assigment/Scenario/ShowMessages/MessagesViewController.swift
+++ involta-job-placement-assigment/Scenario/ShowMessages/MessagesViewController.swift
@@ -32,17 +32,29 @@ class MessagesViewController: UIViewController {
         super.viewDidLoad()
         setupLayout()
         uploadData(to: self.messagesTableView)
+        setupRefreshControl()
     }
+
+    @objc private func didPullToRefresh() {
+        self.cancelCurrentSwipe(of: messagesTableView)
+        self.uploadData(to: messagesTableView)
+    }
+
 
     private func setupLayout() {
         view.addSubview(messagesTableView)
-        view.backgroundColor = .clear
+        view.backgroundColor = .systemBackground
         NSLayoutConstraint.activate([
             messagesTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             messagesTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             messagesTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             messagesTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+    }
+
+    private func setupRefreshControl() {
+        messagesTableView.refreshControl = UIRefreshControl()
+        messagesTableView.refreshControl?.addTarget(self, action: #selector(didPullToRefresh), for: .valueChanged)
     }
 }
 
@@ -56,16 +68,6 @@ extension MessagesViewController: UITableViewDelegate {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let offsetY = scrollView.contentOffset.y
-        print("offsetY: \(offsetY), -scrollView.bounds.size.height / 6: \(-scrollView.bounds.size.height / 6)")
-
-        if offsetY < -scrollView.bounds.size.height / 6 {
-            cancelCurrentSwipe(of: scrollView)
-            uploadData(to: scrollView)
-        }
-    }
-
     func uploadData(to scrollView: UIScrollView) {
         if !isFetching {
             beginBatchFetch {[weak self] isFirstBatch, isCancelledCauseError in
@@ -76,13 +78,18 @@ extension MessagesViewController: UITableViewDelegate {
                     switch isCancelledCauseError {
                     case false:
                         (scrollView as? UITableView)?.insertItemsAtTopWithFixedPosition(
-                            isFirstBatch ? self.pageSize - 1 : self.canFetchMore ? self.pageSize - 2 : self.items.count % self.pageSize,
+                            isFirstBatch ? self.pageSize - 1 : self.canFetchMore ? self.pageSize - 2 : self.items.count % self.pageSize - 1,
                             inSection: Section.messages.rawValue
                         )
                     case true:
-                        self.showErrorAlertController()
+                        if self.canFetchMore {
+                            self.showErrorWhileLoadingAlertController()
+                        } else {
+                            self.showErrorDataIsOverAlertController()
+                        }
                     }
                     self.isFetching = false
+                    self.messagesTableView.refreshControl?.endRefreshing()
                     self.messagesTableView.reloadData()
                 }
             }
@@ -102,9 +109,6 @@ extension MessagesViewController: UITableViewDelegate {
         isFetching = true
 
         if canFetchMore {
-            DispatchQueue.main.async {
-                self.messagesTableView.reloadSections(IndexSet(integer: Section.loading.rawValue), with: .none)
-            }
             networkService.getMessages(offset: items.count) { [weak self] messages, error in
                 guard let self = self else {
                     completion(isFirstBatch, true)
@@ -121,10 +125,13 @@ extension MessagesViewController: UITableViewDelegate {
                     }
                     completion(isFirstBatch, false)
                 }
-            }}
+            }
+        } else {
+            completion(isFirstBatch, true)
+        }
     }
 
-    func showErrorAlertController() {
+    private func showErrorWhileLoadingAlertController() {
         let alertTitle = NSLocalizedString("error.load.title", comment: "")
         let alertMessage = NSLocalizedString("error.load.message", comment: "")
         let cancelActionText = NSLocalizedString("error.load.cancel", comment: "")
@@ -135,9 +142,24 @@ extension MessagesViewController: UITableViewDelegate {
         errorAlertController.addAction(cancelAction)
         let retryAction: UIAlertAction = UIAlertAction(title: retryActionText, style: .default) { [weak self] _ in
             guard let self = self else { return }
+            self.messagesTableView.refreshControl?.beginRefreshing()
             self.uploadData(to: self.messagesTableView)
         }
         errorAlertController.addAction(retryAction)
+        DispatchQueue.main.async {
+            self.present(errorAlertController, animated: true, completion: nil)
+        }
+    }
+
+    private func showErrorDataIsOverAlertController() {
+        let alertTitle = NSLocalizedString("error.data.out.title", comment: "")
+        let alertMessage = NSLocalizedString("error.data.out.message", comment: "")
+        let cancelActionText = NSLocalizedString("error.data.out.cancel", comment: "")
+
+        let errorAlertController: UIAlertController = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
+        let cancelAction: UIAlertAction = UIAlertAction(title: cancelActionText, style: .cancel, handler: nil)
+        errorAlertController.addAction(cancelAction)
+
         DispatchQueue.main.async {
             self.present(errorAlertController, animated: true, completion: nil)
         }
@@ -147,21 +169,16 @@ extension MessagesViewController: UITableViewDelegate {
 extension MessagesViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == Section.loading.rawValue && isFetching {
-            return 1
-        } else if section == Section.messages.rawValue {
+        switch section {
+        case Section.messages.rawValue:
             return items.count
+        default:
+            return 0
         }
-        return 0
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch indexPath.section {
-        case Section.loading.rawValue:
-            let cell = tableView.dequeueReusableCell(withIdentifier: LoadingCell.identifier, for: indexPath) as! LoadingCell
-            cell.selectionStyle = .none
-            cell.startSpinnerAnimation()
-            return cell
         case Section.messages.rawValue:
             let cell = tableView.dequeueReusableCell(withIdentifier: MessageCell.identifier, for: indexPath) as! MessageCell
             cell.messageLabel.preferredMaxLayoutWidth = tableView.bounds.width
@@ -174,7 +191,7 @@ extension MessagesViewController: UITableViewDataSource {
     }
 
     func numberOfSections(in tableView: UITableView) -> Int {
-        2
+        Section.allCases.count
     }
 
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
@@ -195,7 +212,6 @@ extension MessagesViewController: UITableViewDataSource {
 
 }
 
-fileprivate enum Section: Int {
-    case loading = 0
-    case messages
+fileprivate enum Section: Int, CaseIterable {
+    case messages = 0
 }
